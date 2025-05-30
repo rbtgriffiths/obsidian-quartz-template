e129278b0ff78f00fb57499b318369ecfbeef07a
rbtgriffiths <rbgriffiths@protonmail.com>
2025-05-30 09:52:36 -0500

    Update title

--- source/quartz.config.ts
+++ source/quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "TSWNN.COM",
+    pageTitle: "TSWNN",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
